18a31010fca64020a0ef576c531290bf7c7d7ff9
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-02-24 18:08:58 +0530

    feat: add endpoints to retrieve latest products, all admin products and all categories

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -35,3 +35,30 @@ export const newProduct = TryCatch(
     });
   }
 );
+
+export const getLatestProducts = TryCatch(async (req, res, next) => {
+  const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+export const getAllCategories = TryCatch(async (req, res, next) => {
+  const categories = await Product.distinct("category");
+
+  return res.status(200).json({
+    success: true,
+    categories,
+  });
+});
+
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+  const products = await Product.find({});
+
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,10 +1,25 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.middleware.js";
-import { newProduct } from "../controllers/product.controller.js";
+import {
+  getAdminProducts,
+  getAllCategories,
+  getLatestProducts,
+  newProduct,
+} from "../controllers/product.controller.js";
 import { singleUpload } from "../middlewares/multer.middleware.js";
 
 const app = express.Router();
 
+// TO CREATE NEW PRODUCT
 app.post("/new", adminOnly, singleUpload, newProduct);
+
+// TO GET LATEST PRODUCT
+app.get("/latest", getLatestProducts);
+
+// TO GET ALL UNIQUE CATEGORIES
+app.get("/categories", getAllCategories);
+
+// TO GET ALL ADMIN PRODUCTS
+app.get("/admin-products", getAdminProducts);
 
 export default app;
