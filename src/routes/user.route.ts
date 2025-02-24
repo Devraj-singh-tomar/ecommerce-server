e89b8de0d3d33a25872db269dae0601e36b56568
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-02-24 15:16:53 +0530

    feat: users API's

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,19 @@
 import express from "express";
-import { newUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  newUser,
+} from "../controllers/user.controller.js";
 
 const app = express.Router();
 
 app.post("/new", newUser);
+
+app.get("/all", getAllUser);
+
+app.get("/:id", getUser);
+
+app.delete("/:id", deleteUser);
 
 export default app;
