171dae85261c6ca9b7924741e211ab543deb3945
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-02-24 14:55:54 +0530

    created, add error middleware,utility class, new user route, controller, connect db and fix express types

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+
+// IMPORTING ROUTES
+import userRoute from "./routes/user.route.js";
+import { connectDB } from "./utils/feature.js";
+
+const port = 4000;
+
+connectDB();
 
 const app = express();
 
-const port = 4000;
+app.use(express.json());
+
+// USER ROUTE
+app.use("/api/v1/user", userRoute);
+
+app.use(errorMiddleware);
 
 app.get("/hello", (req, res) => {
   res.send("Hello World");
