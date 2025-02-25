966a8660c3e96a0f629502de60716c050a9d3240
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-02-25 14:13:45 +0530

    feat: add myOrders and adimin allOrders endpoints with caching for order retrieval

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -4,6 +4,7 @@ import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.model.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { invalidateCache, reduceStock } from "../utils/feature.js";
+import { myCache } from "../app.js";
 
 export const newOrder = TryCatch(
   async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
@@ -46,3 +47,41 @@ export const newOrder = TryCatch(
     });
   }
 );
+
+export const myOrders = TryCatch(async (req, res, next) => {
+  const { id: user } = req.query;
+
+  const key = `my-orders-${user}`;
+
+  let orders = [];
+
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find({ user: user });
+
+    myCache.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+export const allOrders = TryCatch(async (req, res, next) => {
+  const key = `all-orders`;
+
+  let orders = [];
+
+  if (myCache.has(key)) orders = JSON.parse(myCache.get(key) as string);
+  else {
+    orders = await Order.find().populate("user", "name");
+
+    myCache.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(200).json({
+    success: true,
+    orders,
+  });
+});
--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.middleware.js";
-import { newOrder } from "../controllers/order.controller.js";
+import {
+  allOrders,
+  myOrders,
+  newOrder,
+} from "../controllers/order.controller.js";
 
 const app = express.Router();
 
 app.post("/new", newOrder);
+
+app.get("/my", myOrders);
+
+app.get("/all", adminOnly, allOrders);
 
 export default app;
